b0c92e778c7f3c80701e4474b5ac156a843fd4fa
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-06-30 14:45:01 -0700

    rule boxes only show for selected field

--- src/pages/components/InputSection.tsx
+++ src/pages/components/InputSection.tsx
@@ -1,14 +1,14 @@
 import { Component } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
-const InputSection = ({ field, showRules }) => {
+const InputSection = ({ field, showRules, handleSelect }) => {
   let rules = showRules ? <RulesBox field={field}/> : <div />
   return (
     <section className={styles.username}>
       <h2 className={styles.field_title}>
         {field}
       </h2>
-      <input type="text" className={styles.text_input}/>
+      <input name={field} type="text" className={styles.text_input}  onClick={handleSelect}/>
       {/* <small>
         {field} RULES
       </small> */}
@@ -20,11 +20,11 @@ const InputSection = ({ field, showRules }) => {
 const RulesBox = ({ field }) => {
   const passwordRules = ['between 20 and 50 characters', 'at least 1 Symbol (!,@,#,$,%)', 'at least 1 letter (upper or lower case)', 'at least 1 number (0-9)'];
   const usernameRules = ['between 10 and 50 characters'];
-  const rules = (field === 'username') ? usernameRules : passwordRules;
+  const rules = (field === 'Username') ? usernameRules : passwordRules;
   return (
     <div className={styles.rules}>
       <small>
-        Must contains:
+        Must contain:
         <RulesList rules={rules} field={field}/>
       </small>
     </div>
--- src/pages/components/_CreateAccountForm.tsx
+++ src/pages/components/_CreateAccountForm.tsx
@@ -6,22 +6,29 @@ import CreateAccountHeader from './CreateAccountHeader';
 import InputSection from './InputSection';
 
 
-export default class CreateAccountForm extends Component {
+export default class CreateAccountForm extends Component<{},{ username: string, password: string, selected: string}> {
   constructor(props) {
     super(props);
     this.state = {
       username: '',
       password: '',
+      selected: ''
     }
     this.setState = this.setState.bind(this);
+    this.handleInputFieldSelection = this.handleInputFieldSelection.bind(this);
+  }
+
+  handleInputFieldSelection(event) {
+    this.setState({ selected: event.target.name })
   }
 
   render () {
+    let { selected } = this.state;
     return (
       <form className={`CreateAccountForm ${styles.form}`}>
         <CreateAccountHeader />
-        <InputSection field="username" />
-        <InputSection field="password" />
+        <InputSection field="Username" showRules={selected === 'Username'} handleSelect={this.handleInputFieldSelection}/>
+        <InputSection field="Password" showRules={selected === 'Password'} handleSelect={this.handleInputFieldSelection}/>
         <button>Create Account</button>
       </form>
     )
